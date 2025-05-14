b2048af3fe0ed97fb5b085c8fa6585cd2944e092
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-05-14 15:30:28 +0530

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -1,20 +1,8 @@
 class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        k = 0
-        if(val not in nums):
-            return(len(nums))
-        else:
-            for i in range(len(nums)):
-                if(nums[i] != val):
-                    k += 1
-
-            for i in range(len(nums) - k):
-                nums.remove(val)
-
-                
-        return(k)
+    def removeDuplicates(self, nums):
+        ret = []
+        for i in range(len(nums)):
+            if(nums[i] not in ret):
+                ret.append(nums[i])
+        nums[:] = ret
+        return(len(ret))
